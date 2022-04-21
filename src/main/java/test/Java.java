92a3b902cdd66404bd8ff9e5ab9199575c93c0ff
yukemeng <2869432967@qq.com>
2022-04-21 19:28:06 +0800

    初始化

---
+++ src/main/java/test/Java.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @Auther:yukemeng Date:2022/4/21-04-21-19:19
+ * Description:
+ * version:1.0
+ */
+public class Java {
+
+    public static void main(String[] args) {
+        System.out.println("=========================");
+    }
+}
